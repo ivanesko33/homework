e7ad7e95b3ccf97e2f7b83644f44e9b2f3c99723
Ivan V. <ivanesko3389@mail.ru>
2026-08-02 15:12:44 +0000

    each block of widgets in subdef, make block name bold. fix bugs

--- homework5/main.py
+++ homework5/main.py
@@ -7,7 +7,7 @@ class CurrencyApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Курс обмена валюты")
-        self.root.geometry("360x380")
+        self.root.geometry("360x450")
 
         # Данные
         self.currency_names = {
@@ -29,58 +29,99 @@ class CurrencyApp:
         self._create_widgets()
 
     def _create_widgets(self):
-        # Метка для базовой валюты
-        label_base1 = tk.Label(self.root, text="Базовая валюта")
-        label_base1.pack(padx=10, pady=10)
-        self.combobox_base1 = ttk.Combobox(
-            self.root,
-            values=self.popular_currencies,
-            state="readonly"
-        )
-        self.combobox_base1.current(0)  # По умолчанию EUR
-        self.combobox_base1.pack(padx=10, pady=10)
+        def create_widget_base1():
+            """ ПодВиджет Первая базовая валюта """
+            self.label_base1 = tk.Label(
+                self.root,
+                text="Базовая валюта",
+                font=("Arial", 14, "bold"))
+            self.label_base1.pack(padx=10, pady=10)
+            self.combobox_base1 = ttk.Combobox(
+                self.root,
+                values=self.popular_currencies,
+                state="readonly"
+            )
+            self.combobox_base1.current(0)  # По умолчанию EUR
+            self.combobox_base1.pack(padx=10, pady=10)
+            def on_combobox_base1_change(event):
+                selected = self.combobox_base1.get()
+                self.label_base_name.config(
+                    text=self.currency_names[selected])
 
-        def on_combobox_base1_change(event):
-            # Получаем текущее значение из Combobox
-            selected = self.combobox_base1.get()
-            # Обновляем текст в Label
-            label_base_name.config(text=self.currency_names[selected])
-        label_base_name = tk.Label(
-            self.root,
-            text=self.currency_names[self.combobox_base1.get()]
-        )
-        label_base_name.pack(padx=10, pady=10)
+            self.label_base_name = tk.Label(
+                self.root,
+                text=self.currency_names[self.combobox_base1.get()]
+            )
+            self.label_base_name.pack(padx=10, pady=10)
 
-        self.combobox_base1.bind("<<ComboboxSelected>>",
-                               on_combobox_base1_change)
-        self.combobox_base1.get()
+            self.combobox_base1.bind(
+                "<<ComboboxSelected>>",
+                on_combobox_base1_change)
 
+        create_widget_base1()
 
-        # Метка для второй базовой валюты
-        label_base2 = tk.Label(self.root, text="Вторая базовая валюта")
-        label_base2.pack(padx=10, pady=10)
+        def create_widget_base2():
+            """ Второй базовой валюты """
+            self.label_base2 = tk.Label(
+                self.root,
+                text="Вторая базовая валюта",
+                font=("Arial", 14, "bold")
+            )
+            self.label_base2.pack(padx=10, pady=10)
+            self.combobox_base2 = ttk.Combobox(
+                self.root,
+                values=self.popular_currencies,
+                state="readonly"
+            )
+            self.combobox_base2.current(1)  # По умолчанию JPY
+            self.combobox_base2.pack(padx=10, pady=10)
 
-        # Выпадающий список для второй базовой волюты
-        self.combobox_base2 = ttk.Combobox(
-            self.root,
-            values=self.popular_currencies,
-            state="readonly"
-        )
-        self.combobox_base2.current(1)  # По умолчанию JPY
-        self.combobox_base2.pack(padx=10, pady=10)
+            def on_combobox_base2_change(event):
+                selected = self.combobox_base2.get()
+                self.label_base2_name.config(
+                    text=self.currency_names[selected])
 
-        # Метка для целевой валюты
-        label_target = tk.Label(self.root, text="Целевая валюта")
-        label_target.pack(padx=10, pady=10)
+            self.label_base2_name = tk.Label(
+                self.root,
+                text=self.currency_names[self.combobox_base2.get()]
+            )
+            self.label_base2_name.pack(padx=10, pady=10)
 
-        # Выпадающий список для второй базовой волюты
-        self.combobox_target = ttk.Combobox(
-            self.root,
-            values=self.popular_currencies,
-            state="readonly"
-        )
-        self.combobox_target.current(7)  # По умолчанию RUB
-        self.combobox_target.pack(padx=10, pady=10)
+            self.combobox_base2.bind("<<ComboboxSelected>>",
+                                     on_combobox_base2_change)
+
+        create_widget_base2()
+
+        def create_widget_target():
+            """ Целевой валюты """
+            self.label_target = tk.Label(
+                self.root,
+                text="Целевая валюта",
+                font=("Arial", 14, "bold")
+            )
+            self.label_target.pack(padx=10, pady=10)
+            self.combobox_target = ttk.Combobox(
+                self.root,
+                values=self.popular_currencies,
+                state="readonly"
+            )
+            self.combobox_target.current(7)  # По умолчанию RUB
+            self.combobox_target.pack(padx=10, pady=10)
+
+            def on_combobox_target_change(event):
+                selected = self.combobox_target.get()
+                self.label_target_name.config(
+                    text=self.currency_names[selected])
+            self.label_target_name = tk.Label(
+                self.root,
+                text=self.currency_names[self.combobox_target.get()]
+            )
+            self.label_target_name.pack(padx=10, pady=10)
+
+            self.combobox_target.bind("<<ComboboxSelected>>",
+                                     on_combobox_target_change)
+
+        create_widget_target()
 
         # Кнопка
         btn = tk.Button(
@@ -107,8 +148,8 @@ class CurrencyApp:
                 mb.showinfo(
                     f"Курс обмена",
                     (f"Курс к доллару:"
-                        f"{exchange_rate:.1f} {code} за 1 "
-                        f"доллар")
+                     f"{exchange_rate:.1f} {code} за 1 "
+                     f"доллар")
                 )
             else:
                 mb.showerror(
