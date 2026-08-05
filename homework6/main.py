e7c91158808fe9dc611e9771f5156eb99c420d53
Ivan V. <ivanesko3389@mail.ru>
2026-08-05 18:09:46 +0000

    change icon image size, add button to refresh inforamtion with icon

--- homework6/main.py
+++ homework6/main.py
@@ -9,13 +9,22 @@ CRYPTO = {
     "binancecoin": "Binance Coin (BNB)",
     "ripple": "Ripple (XRP)"
 }
-# utl строка для get запроса
-param = ",".join(CRYPTO.keys())
-url = (f"https://api.coingecko.com/api/v3/simple/price?ids"
-       f"={param}&vs_currencies=usd")
+# название файла с иконкой приложения и для кнопки
+ICON_FILE = 'cript_coin.png'
+# url строка для get запроса
+PARAM = ','.join(CRYPTO.keys())
+URL = (f"https://api.coingecko.com/api/v3/simple/price?ids"
+       f"={PARAM}&vs_currencies=usd")
 # список курсы популярных криптовалют к доллару США
 result = list()
-
+# иконка для криптовалют, для основного окна и для кнопки
+# главное окно
+root = tk.Tk()
+root.title("курсы популярных криптовалют")
+root.geometry("400x450")
+# установка иконки окна
+icon = tk.PhotoImage(file=ICON_FILE)
+root.iconphoto(False, icon)
 
 def parsing_data(data):
     """ GET запрос к coingecko для криптовалют в CRYPTO_MAP """
@@ -43,26 +52,38 @@ def show_coint_info(coin: dict) -> str:
     """ словарь описания монеты в строку """
     return f'{coin["display_name"]}: {coin["price_usd"]}'
 
+def on_click():
+    print("Кнопка нажата!")
+
+
 try:
     # Установка таймаута на случай проблем со связью
-    response = requests.get(url, timeout=10)
+    response = requests.get(URL, timeout=10)
     if response.status_code == 200:
         result  = parsing_data(response.json())
-        # Создаём главное окно
-        root = tk.Tk()
-        root.title("курсы популярных криптовалют")
-        root.geometry("400x450")
-        import tkinter as tk
-        # Загрузка и установка изображение для иконки окна
-        icon = tk.PhotoImage(file="cript_coin.png")
-        root.iconphoto(False, icon)
+
 
         for coin in result:
-            label = tk.Label(root, text=show_coint_info(coin),
-                             wraplength=350,
-                            justify="left")
+            label = tk.Label(
+                root,
+                text=show_coint_info(coin),
+                wraplength=350,
+                justify="left",
+                font=("Arial", 14)
+            )
             label.pack(pady=20)
-        # Запуск цикла
+        # кнопка для обновления
+        btn = tk.Button(
+            root,
+            text="Обновить",
+            image=icon,
+            compound=tk.LEFT,
+            command=on_click,
+            padx=10,
+            pady=5
+        )
+        btn.pack(pady=20)
+
         root.mainloop()
 
         for item in result:
